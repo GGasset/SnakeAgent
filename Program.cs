de3a77acd06b5e5abd1d51174c2f3a58b340b8a0
Germán Gasset <gassetgerman@gmail.com>
2022-03-20 21:25:50 +0100

    started with human supervision questions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NN;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,22 @@ namespace SnakeNN
     {
         static void Main(string[] args)
         {
+            int[] networkTopology = { 45, 27, 16, 4 };
+            int gridSize = 25;
 
+            SnakeGame snakeGame = new SnakeGame(gridSize);
+            ReinforcementAgent agent = new ReinforcementAgent(gridSize * gridSize, networkTopology, .1, NN.Libraries.Activation.ActivationFunctions.Relu);
+
+            
+        }
+
+        void HumanSupervisionQuestions(out bool visualize, out int generationsWithoutQuestions, out bool saveToFile)
+        {
+            Console.Write("Want to visualize current generation? Y/N - ");
+            visualize = Console.ReadLine().ToUpperInvariant() == "Y"; ;
+            Console.WriteLine();
+            Console.Write("How many generations are next for training? - ");
+            generationsWithoutQuestions = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
--- SnakeGame.cs
+++ SnakeGame.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneticSnake
+namespace SnakeNN
 {
     internal class SnakeGame
     {
